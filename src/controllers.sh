6a6e104b77f81bd5d6307e08e390dd98b48ea3c6
Jacob <jacob.folch@gmail.com>
2025-02-03 14:42:37 +0100

    fix(username): Cancel on change username form didn't work as expected

--- src/controllers.sh
+++ src/controllers.sh
@@ -42,9 +42,7 @@ user_name_controller() {
     new_username="$(user_name_form "$error" | xargs)"
 
     if [ -z "$new_username" ];then
-      error "empty username not allowed!"
-      error="Empty username"
-      continue
+      return
     fi
 
     # Parse input
